7e89a8265965f2dece9693322b649d220e15e51f
karl <karlhadwen@gmail.com>
2020-04-22 20:57:24 +0100

    feat: src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const inputFocusRef = useRef();
+  const counterRef = useRef(0);
+
+  function incrementCounterRef() {
+    counterRef.current += 1;
+  }
+
+  useEffect(() => {
+    const { current } = inputFocusRef;
+
+    function focusInput() {
+      console.log('Focused on the input with DOM value: ', inputFocusRef);
+      console.log('Current value is: ', current);
+    }
+
+    current.addEventListener('focus', focusInput);
+
+    return () => {
+      current.removeEventListener('focus', focusInput);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <p>State count: {count}</p>
+      <input
+        type="text"
+        ref={inputFocusRef}
+        defaultValue="Click inside...I dare you"
+      />
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Increment Count
+      </button>
+
+      <p className="mt">Ref Count: {counterRef.current}</p>
+      <button type="button" onClick={incrementCounterRef}>
+        Increment Ref Count
+      </button>
+    </main>
   );
 }
 
